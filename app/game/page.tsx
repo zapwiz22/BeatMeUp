68a487325693ad945e3891bfae335f2ba2ed1134
zapwiz22 <kumar.jayant1916@gmail.com>
2025-07-21 15:31:36 +0530

    fixing client search params issue

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,13 +1,20 @@
 "use client";
 import GameCanvas from "@/components/GameCanvas";
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
-export default function Game() {
+function GameContent() {
   const searchParams = useSearchParams();
   const captchaToken = searchParams.get("captchaToken") || "";
+  return <GameCanvas captchaToken={captchaToken} />;
+}
+
+export default function Game() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center p-4">
-      <GameCanvas captchaToken={captchaToken} />
+      <Suspense>
+        <GameContent />
+      </Suspense>
     </main>
   );
 }
